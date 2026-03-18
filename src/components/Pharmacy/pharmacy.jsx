2fc16e129dc8aa78f505a8e5ac35f157e2636662
Sushmitha K <sushmithagowda712@.gmail.com>
2026-03-18 14:10:03 +0530

    up to date

--- src/components/Pharmacy/pharmacy.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Pharmacy.module.css'
-
-export default function Pharmacy() {
-  return (
-    <section className={styles.container}>
-      <h2>Pharmacy Section</h2>
-    </section>
-  )
-}
